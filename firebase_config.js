a8a3cdcc8e78237d7615c32d25bb0c93ccfdc1e6
soportechagres-del <soportechagres@gmail.com>
2026-01-19 14:49:27 -0500

    Add files via upload

---
+++ firebase_config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQqADe-f3dSNqbe4mtogUWrr9fzhevFY0",
+  authDomain: "prestamos-2141a.firebaseapp.com",
+  projectId: "prestamos-2141a",
+  storageBucket: "prestamos-2141a.firebasestorage.app",
+  messagingSenderId: "609384737583",
+  appId: "1:609384737583:web:0825a6ee1a1db2080a8fda"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
